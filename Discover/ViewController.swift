9267d3a49a12d46cbd57d1767d1dc0f55bf90e73
nataliemurillo <nataliemurillo23@yahoo.com>
2017-07-18 13:21:25 -0700

    added a pulley effect
    there is a pull view controller that is full of methods

--- Discover/ViewController.swift
+++ Discover/ViewController.swift
@@ -8,32 +8,54 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var mapViewController: MKMapView!
     
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        
+        let mapSpan:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let mapRegion:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, mapSpan)
+        mapViewController.setRegion(mapRegion, animated: true)
+        
+        self.mapViewController.showsUserLocation = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let mapSpan:MKCoordinateSpan = MKCoordinateSpanMake(00.1, 00.1)
-        let centerLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(37.812643, -122.2663986)
-        let mapRegion:MKCoordinateRegion = MKCoordinateRegionMake(centerLocation, mapSpan)
-        mapViewController.setRegion(mapRegion, animated: true)
-        
-        let annotation = MKPointAnnotation()
-        
-        annotation.coordinate = centerLocation
-        annotation.title = "AYOOOOO"
-        annotation.subtitle = "it works!!!!!!!"
-        mapViewController.addAnnotation(annotation)
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+//        
+//        let mapSpan:MKCoordinateSpan = MKCoordinateSpanMake(centerLocation.coordinate.latitude, centerLocation.coordinate.longitude)
+//        let centerLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(37.812643, -122.2663986)
+//        let mapRegion:MKCoordinateRegion = MKCoordinateRegionMake(centerLocation, mapSpan)
+//        mapViewController.setRegion(mapRegion, animated: true)
+//        
+//        let annotation = MKPointAnnotation()
+//        
+//        annotation.coordinate = centerLocation
+//        annotation.title = "AYOOOOO"
+//        annotation.subtitle = "it works!!!!!!!"
+//        mapViewController.addAnnotation(annotation)
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    
+}
